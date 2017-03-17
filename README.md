74e5681d50f71c24105c8fdefa1080c3f57aec93
j.guegan-ihu <j.guegan-ihu@icm-institute.org>
2017-03-17 13:21:59 +0100

    added .nojekyll

--- README.md
+++
@@ -1,4 +0,0 @@
-# Formation-Rrrr
-formation-R-mars-2017
-
-Le site de la formation se trouve à l'adresse suivante : https://pf-bb.github.io/Formation-Rrrr/
--- docs/source/TP2/exo1/pathview_correction.R
+++
@@ -1,6 +0,0 @@
-library(pathview)
-genes = read.table("myGenes.csv", sep=";", header=TRUE, stringsAsFactors = FALSE)
-rownames(genes) = genes[,2]
-pathwayID = "hsa04012"
-geneIDTYPE = "SYMBOL"
-pathview(genes[,2], pathway.id = pathwayID, gene.idtype = geneIDTYPE,plot.col.key= FALSE)
